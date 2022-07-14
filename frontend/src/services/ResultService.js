dde12a381b9febbf75cb92fd5c3094ba7f5e1ee0
Brennan <tanjialebrennan@gmail.com>
2022-07-15 00:07:30 +0800

    Update ResultService.js with new GET method
    GET method that will retrieve the rankings of each team in their respective groups

--- frontend/src/services/ResultService.js
+++ frontend/src/services/ResultService.js
@@ -1,3 +1,11 @@
-class ResultService {}
+import axios from "axios";
+
+const RESULT_REST_API_URL = "http://localhost:8080/api/v1/results/";
+
+class ResultService {
+  getResults() {
+    return axios.get(RESULT_REST_API_URL);
+  }
+}
 
 export default new ResultService();
