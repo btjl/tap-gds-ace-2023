37dc90a8771d765bd7c31d8311d0d2c8c6c3b4d5
Brennan <tanjialebrennan@gmail.com>
2022-07-14 21:00:34 +0800

    Create TeamRepository.java
    Added data access layer for team model

---
+++ backend/src/main/java/com/btjl/tapgdsace2023/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package com.btjl.tapgdsace2023.repository;
+
+import com.btjl.tapgdsace2023.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Integer> {
+
+}
