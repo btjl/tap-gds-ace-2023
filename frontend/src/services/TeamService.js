a846dc157a9e46528842ff7d420a899cc4e0903c
Brennan <tanjialebrennan@gmail.com>
2022-07-14 20:53:00 +0800

    Update TeamService.js with helper function
    Processes the text area input into an array of Team object models

--- frontend/src/services/TeamService.js
+++ frontend/src/services/TeamService.js
@@ -1,3 +1,22 @@
-class TeamService {}
+class TeamService {
+  processTextAreaInput(textAreaInput) {
+    const splitInputArray = textAreaInput.split("\n");
+
+    const teamsArray = [];
+    for (let i = 0; i < splitInputArray.length; i++) {
+      let [curName, curRegistrationDate, curGroup] =
+        splitInputArray[i].split(" ");
+
+      teamsArray.push({
+        id: i,
+        name: curName,
+        registrationDate: curRegistrationDate,
+        groupNumber: curGroup,
+      });
+    }
+
+    return teamsArray;
+  }
+}
 
 export default new TeamService();
