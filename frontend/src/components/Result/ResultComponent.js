6d66e0ed0c4eb750c21cc22c9c714d5f90877057
Brennan <tanjialebrennan@gmail.com>
2022-07-15 00:28:38 +0800

    Integrate TableRankingComponent into results

--- frontend/src/components/Result/ResultComponent.js
+++ frontend/src/components/Result/ResultComponent.js
@@ -1,5 +1,6 @@
 import { React, useEffect, useState } from "react";
 import ResultService from "../../services/ResultService";
+import TableRankingComponent from "./TableRankingComponent";
 import { Button } from "react-bootstrap";
 
 const ResultComponent = () => {
@@ -22,9 +23,10 @@ const ResultComponent = () => {
   return (
     <div className="container">
       <h1>Rankings</h1>
-      <h2>Group 1</h2>
+      <TableRankingComponent data={resultObj["group1Rankings"]} />
 
       <h2>Group 2</h2>
+      <TableRankingComponent data={resultObj["group2Rankings"]} />
 
       <Button variant="btn btn-danger" type="submit">
         START OVER - CLEAR ALL DATA
--- frontend/src/components/Result/TableRankingComponent.js
+++ frontend/src/components/Result/TableRankingComponent.js
@@ -1,7 +1,49 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
 
-const TableRankingComponent = () => {
-  return <div>TableRankingComponent</div>;
+const TableRankingComponent = (props) => {
+  const teamRankings = props.data;
+
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Team Name</th>
+            <th>Total Match Point</th>
+            <th>Total Goals</th>
+            <th>Total Alternate Match Points</th>
+            <th>Registration Date In Days - Smaller equals earlier</th>
+            <th>Qualify for next round?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teamRankings &&
+            teamRankings.map(
+              (
+                {
+                  teamName,
+                  totalMatchPoint,
+                  totalGoals,
+                  totalAlternateMatchPoint,
+                  registrationDateInDays,
+                },
+                index
+              ) => (
+                <tr key={index}>
+                  <td>{teamName}</td>
+                  <td>{totalMatchPoint}</td>
+                  <td>{totalGoals}</td>
+                  <td>{totalAlternateMatchPoint}</td>
+                  <td>{registrationDateInDays}</td>
+                  <td>{index <= 3 ? "YES" : "NO"}</td>
+                </tr>
+              )
+            )}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default TableRankingComponent;
