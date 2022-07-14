18ecac014aa6db9f7e2a5166d75c4e306bfea9d6
Brennan <tanjialebrennan@gmail.com>
2022-07-15 00:23:59 +0800

    Integrate ResultComponent.js with backend
    Added calling of the backend service on page load

--- frontend/src/components/Result/ResultComponent.js
+++ frontend/src/components/Result/ResultComponent.js
@@ -1,7 +1,36 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+import ResultService from "../../services/ResultService";
+import { Button } from "react-bootstrap";
 
 const ResultComponent = () => {
-  return <div>ResultComponent</div>;
+  const [resultObj, setResultObj] = useState({});
+
+  useEffect(() => {
+    getAllResults();
+  }, []);
+
+  const getAllResults = () => {
+    ResultService.getResults()
+      .then((response) => {
+        setResultObj(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="container">
+      <h1>Rankings</h1>
+      <h2>Group 1</h2>
+
+      <h2>Group 2</h2>
+
+      <Button variant="btn btn-danger" type="submit">
+        START OVER - CLEAR ALL DATA
+      </Button>
+    </div>
+  );
 };
 
 export default ResultComponent;
