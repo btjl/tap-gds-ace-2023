739e2230cf31146fe21a2ecac939cd7592b6f02a
Brennan <tanjialebrennan@gmail.com>
2022-07-15 00:00:24 +0800

    Create ResultController.java
    REST controller that contains HTTP methods dealing with the overall results

---
+++ backend/src/main/java/com/btjl/tapgdsace2023/controller/ResultController.java
@@ -0,0 +1,11 @@
+package com.btjl.tapgdsace2023.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/results/")
+public class ResultController {
+}
