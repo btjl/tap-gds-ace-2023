0ed8800d150d936cb918d2bb2abd96164247675a
Brennan <tanjialebrennan@gmail.com>
2022-07-14 21:31:26 +0800

    Create FootballMatchScores.java
    Object that models a match, the teams that participated and their respective scores

---
+++ backend/src/main/java/com/btjl/tapgdsace2023/dto/FootballMatchScores.java
@@ -0,0 +1,14 @@
+package com.btjl.tapgdsace2023.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FootballMatchScores {
+
+    private String team1Name;
+    private String team2Name;
+    private Integer team1Score;
+    private Integer team2Score;
+}
