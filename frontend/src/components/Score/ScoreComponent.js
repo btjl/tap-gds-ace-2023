243860ed2a40236650d7b87efd832b7aa0ec062f
Brennan <tanjialebrennan@gmail.com>
2022-07-14 21:16:45 +0800

    Update ScoreComponent.js with form input
    Text area form input and submit button

--- frontend/src/components/Score/ScoreComponent.js
+++ frontend/src/components/Score/ScoreComponent.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 const ScoreComponent = () => {
-  return <div>ScoreComponent</div>;
+  return (
+    <div>
+      <h1>Score page</h1>
+      <div className="container">
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Scores Form</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={12}
+              placeholder="<Team A name> <Team B name> <Team A goals scored> <Team B goals scored>&#10;<Team B name> <Team C name> <Team B goals scored> <Team C goals scored>&#10;<Team C name> <Team D name> <Team C goals scored> <Team D goals scored>"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default ScoreComponent;
