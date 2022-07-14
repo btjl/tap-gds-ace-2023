8a831660449bdb122b85a37babacc9888d716789
Brennan <tanjialebrennan@gmail.com>
2022-07-14 20:46:53 +0800

    Update TeamComponent.js with form input
    Text area form input and submit button

--- frontend/src/components/Team/TeamComponent.js
+++ frontend/src/components/Team/TeamComponent.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 const TeamComponent = () => {
-  return <div>TeamComponent</div>;
+  return (
+    <div>
+      <h1>Team Registration</h1>
+      <div className="container">
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Team Information Form</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={12}
+              placeholder="<Team A name> <Team A registration date in DD/MM> <Team A group number>&#10;<Team B name> <Team B registration date in DD/MM> <Team B group number>&#10;<Team C name> <Team C registration date in DD/MM> <Team C group number>&#10;..."
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default TeamComponent;
