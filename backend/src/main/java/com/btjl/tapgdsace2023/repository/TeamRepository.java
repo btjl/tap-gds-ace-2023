0f09c30ac0467d1b186db538c3ea477e627ebfbb
Brennan <tanjialebrennan@gmail.com>
2022-07-14 21:53:51 +0800

    Update TeamRepository.java with custom query
    Custom query method for finding a team by name

--- backend/src/main/java/com/btjl/tapgdsace2023/repository/TeamRepository.java
+++ backend/src/main/java/com/btjl/tapgdsace2023/repository/TeamRepository.java
@@ -2,7 +2,11 @@ package com.btjl.tapgdsace2023.repository;
 
 import com.btjl.tapgdsace2023.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TeamRepository extends JpaRepository<Team, Integer> {
+
+    @Query(value = "select * from teams where name = ?1", nativeQuery = true)
+    Team getTeamByName(String name);
 
 }
