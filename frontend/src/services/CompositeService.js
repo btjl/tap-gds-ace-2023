657968a6044ae2561a9174276fac2c10318abfd6
Brennan <tanjialebrennan@gmail.com>
2022-07-15 00:43:14 +0800

    Update CompositeService.js with new DELETE method
    Calls the backend to delete all records in the database

--- frontend/src/services/CompositeService.js
+++ frontend/src/services/CompositeService.js
@@ -1,3 +1,11 @@
-class CompositeService {}
+import axios from "axios";
+
+const COMPOSITE_REST_API_URL = "http://localhost:8080/api/v1/composite/";
+
+class CompositeService {
+  clearData() {
+    return axios.delete(COMPOSITE_REST_API_URL + "clear");
+  }
+}
 
 export default new CompositeService();
