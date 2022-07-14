13692da5f3fc6e23f4ea321e31a6a0ae5f0003f5
Brennan <tanjialebrennan@gmail.com>
2022-07-14 19:33:43 +0800

    Backend setup
    Starter files initialised by IntelliJ IDE

---
+++ backend/src/main/java/com/btjl/tapgdsace2023/Tapgdsace2023Application.java
@@ -0,0 +1,13 @@
+package com.btjl.tapgdsace2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tapgdsace2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tapgdsace2023Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/btjl/tapgdsace2023/Tapgdsace2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btjl.tapgdsace2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tapgdsace2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
