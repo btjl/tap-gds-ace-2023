811fd70863c917f72f87daaa3e6648392e3d2eaa
Brennan <tanjialebrennan@gmail.com>
2022-07-14 23:27:20 +0800

    Integrate ScoreComponent.js with backend
    Added event handling upon form submission using ScoreService.js as the connector to the backend

--- frontend/src/components/Score/ScoreComponent.js
+++ frontend/src/components/Score/ScoreComponent.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { React, useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import ScoreService from "../../services/ScoreService";
 
 const ScoreComponent = () => {
+  const [textAreaInput, setTextAreaInput] = useState("");
+  const navigate = useNavigate();
+
+  const saveScoresInformation = (e) => {
+    e.preventDefault();
+
+    const matchScores = ScoreService.processTextAreaInput(textAreaInput);
+
+    ScoreService.saveMatchScores(matchScores)
+      .then((response) => {
+        navigate("/results", response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>Score page</h1>
@@ -13,9 +32,14 @@ const ScoreComponent = () => {
               as="textarea"
               rows={12}
               placeholder="<Team A name> <Team B name> <Team A goals scored> <Team B goals scored>&#10;<Team B name> <Team C name> <Team B goals scored> <Team C goals scored>&#10;<Team C name> <Team D name> <Team C goals scored> <Team D goals scored>"
+              onChange={(e) => setTextAreaInput(e.target.value)}
             />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={(e) => saveScoresInformation(e)}
+          >
             Submit
           </Button>
         </Form>
