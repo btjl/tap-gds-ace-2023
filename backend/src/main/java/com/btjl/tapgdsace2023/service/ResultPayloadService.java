44335157bdd5ad9c1391130165248440a0d1f5fd
Brennan <tanjialebrennan@gmail.com>
2022-07-14 23:46:13 +0800

    Add composite method in ResultPayloadService.java
    Retrieves all the information for teams in the respective groups, calculates the rankings among the groups, creates a new ResultPayload object from the result and return it

--- backend/src/main/java/com/btjl/tapgdsace2023/service/ResultPayloadService.java
+++ backend/src/main/java/com/btjl/tapgdsace2023/service/ResultPayloadService.java
@@ -1,8 +1,40 @@
 package com.btjl.tapgdsace2023.service;
 
+import com.btjl.tapgdsace2023.model.ResultPayload;
+import com.btjl.tapgdsace2023.model.TeamFootballMatch;
+import com.btjl.tapgdsace2023.model.TeamRanking;
+import com.btjl.tapgdsace2023.repository.TeamFootballMatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ResultPayloadService {
 
+    @Autowired
+    TeamFootballMatchRepository teamFootballMatchRepository;
+
+    public ResultPayload getGroupRankings() {
+
+        List<TeamRanking> group1Rankings, group2Rankings;
+
+        List<TeamFootballMatch> teamFootballMatchList = teamFootballMatchRepository.findAll();
+
+        List<TeamFootballMatch> group1 = getTeamsInGroup(teamFootballMatchList, 1);
+        List<TeamFootballMatch> group2 = getTeamsInGroup(teamFootballMatchList, 2);
+
+        group1Rankings = calculateGroupRankingsForTeamsInGroup(group1);
+        group2Rankings = calculateGroupRankingsForTeamsInGroup(group2);
+
+        return new ResultPayload(group1Rankings, group2Rankings);
+    }
+
+    private List<TeamRanking> calculateGroupRankingsForTeamsInGroup(List<TeamFootballMatch> group1) {
+        return null;
+    }
+
+    private List<TeamFootballMatch> getTeamsInGroup(List<TeamFootballMatch> teamFootballMatchList, int i) {
+        return null;
+    }
 }
