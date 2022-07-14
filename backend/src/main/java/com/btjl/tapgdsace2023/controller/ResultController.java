b26fe96aab73ac9aeddbcb234691a2939e1defe2
Brennan <tanjialebrennan@gmail.com>
2022-07-15 00:02:35 +0800

    Add GET method to ResultController.java
    Calls ResultPayloadService.java to calculate the team's group rankings and returns the ResultPayload

--- backend/src/main/java/com/btjl/tapgdsace2023/controller/ResultController.java
+++ backend/src/main/java/com/btjl/tapgdsace2023/controller/ResultController.java
@@ -1,6 +1,10 @@
 package com.btjl.tapgdsace2023.controller;
 
+import com.btjl.tapgdsace2023.model.ResultPayload;
+import com.btjl.tapgdsace2023.service.ResultPayloadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/results/")
 public class ResultController {
+
+    @Autowired
+    ResultPayloadService resultPayloadService;
+
+    @GetMapping
+    public ResultPayload getTeamRankings() {
+        return resultPayloadService.getGroupRankings();
+    }
+
 }
