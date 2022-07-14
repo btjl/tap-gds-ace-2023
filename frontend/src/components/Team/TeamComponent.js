8666fa2ee9b6635535da8b7c0e8bcd0fd0efb1bb
Brennan <tanjialebrennan@gmail.com>
2022-07-14 21:14:44 +0800

    Integrate TeamComponent.js with backend
    Added event handling upon form submission using TeamService.js as the connector to the backend

--- frontend/src/components/Team/TeamComponent.js
+++ frontend/src/components/Team/TeamComponent.js
@@ -1,7 +1,29 @@
-import React from "react";
+import { React, useState } from "react";
+import TeamService from "../../services/TeamService";
 import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const TeamComponent = () => {
+  const [textAreaInput, setTextAreaInput] = useState("");
+  const navigate = useNavigate();
+
+  const saveTeamsInformation = (e) => {
+    e.preventDefault();
+
+    const teamsInformationObject =
+      TeamService.processTextAreaInput(textAreaInput);
+
+    console.log(teamsInformationObject);
+
+    TeamService.registerTeams(teamsInformationObject)
+      .then((response) => {
+        navigate("/scores");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <h1>Team Registration</h1>
@@ -13,9 +35,14 @@ const TeamComponent = () => {
               as="textarea"
               rows={12}
               placeholder="<Team A name> <Team A registration date in DD/MM> <Team A group number>&#10;<Team B name> <Team B registration date in DD/MM> <Team B group number>&#10;<Team C name> <Team C registration date in DD/MM> <Team C group number>&#10;..."
+              onChange={(e) => setTextAreaInput(e.target.value)}
             />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={(e) => saveTeamsInformation(e)}
+          >
             Submit
           </Button>
         </Form>
