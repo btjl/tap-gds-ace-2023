d2561e96d927b5630750f290a5a00efeed196e96
Brennan <tanjialebrennan@gmail.com>
2022-07-14 21:26:36 +0800

    Update ScoreService.js with helper function
    Processes the text area input into an array of objects that models a match, the teams that participated and their respective scores

--- frontend/src/services/ScoreService.js
+++ frontend/src/services/ScoreService.js
@@ -1,3 +1,21 @@
-class ScoreService {}
+class ScoreService {
+  processTextAreaInput(textAreaInput) {
+    const splitInputArray = textAreaInput.split("\n");
+
+    const matchScoresArray = [];
+    for (let i = 0; i < splitInputArray.length; i++) {
+      let [team1, team2, score1, score2] = splitInputArray[i].split(" ");
+
+      matchScoresArray.push({
+        team1Name: team1,
+        team2Name: team2,
+        team1Score: score1,
+        team2Score: score2,
+      });
+    }
+
+    return matchScoresArray;
+  }
+}
 
 export default new ScoreService();
