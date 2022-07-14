1e4501d086bd749f224169a55df70c33667cc99d
Brennan <tanjialebrennan@gmail.com>
2022-07-14 21:48:59 +0800

    Create ScoreController.java
    REST controller that contains HTTP methods dealing with football match score payloads

---
+++ backend/src/main/java/com/btjl/tapgdsace2023/controller/ScoreController.java
@@ -0,0 +1,12 @@
+package com.btjl.tapgdsace2023.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/v1/scores/")
+public class ScoreController {
+}
